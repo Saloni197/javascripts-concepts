cc12e4c7c64329bf6ef7a24955f56c8e6b16ef44
Saloni197 <Kavita.Goyal@technocodz.com>
2021-05-24 08:36:23 +0530

    numFunc
    Signed-off-by: Saloni197 <Kavita.Goyal@technocodz.com>

---
+++ numbers/roundingFunctions.js
@@ -0,0 +1,28 @@
+let num1 = 2.65;
+let num2 = -3.12;
+
+// print round down numbers
+console.log(Math.floor(num1));     
+console.log(Math.floor(num2));     
+
+// print round up numbers
+console.log(Math.ceil(num1));     
+console.log(Math.ceil(num2));     
+
+// print round to nearest integer
+console.log(Math.round(num1));     
+console.log(Math.round(num2)); 
+
+// remove anything after decimal
+console.log(Math.trunc(num1));     
+console.log(Math.trunc(num2)); 
+
+//print any random number
+console.log(Math.random());        
+
+//print minimum and maximum number
+console.log(Math.min(3,1,10,-6));
+console.log(Math.max(3,1,10,-6));
+
+//print power of any number
+console.log(Math.pow(3,4));
